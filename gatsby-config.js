1243db20ffb24b67aaaf236a5457d75cd374af5d
Filip Mares <fmares@sessionm.com>
2022-03-04 16:57:53 +0100

    finalization

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,11 +1,27 @@
+const pkg = require('./package.json');
+const DESC = `Čeho všeho jsme byli svědky za posledních 5 let úřadování českého prezidenta na hradě? Nabízíme přehled novinových zpráv s největšími přešlapy na časové ose.`;
+
 module.exports = {
   siteMetadata: {
-    title: "Miloš Zeman: pět let zpět",
-    description: "Největší přešlapy současného prezidenta na časové ose.",
-    author: "@etylsarin",
-    siteUrl: "https://www.petletzpet.cz/",
+    title: pkg.description,
+    description: DESC,
+    author: pkg.author,
+    siteUrl: pkg.homepage,
   },
   plugins: [
+    {
+      resolve: "gatsby-plugin-manifest",
+      options: {
+        name: pkg.description,
+        short_name: pkg.name,
+        description: DESC,
+        start_url: `/`,
+        background_color: `#fbfbfb`,
+        theme_color: `#fbfbfb`,
+        display: `standalone`,
+        icon: "src/images/icon.png",
+      },
+    },
     'gatsby-plugin-sass',
     {
       resolve: 'gatsby-plugin-svgr',
@@ -48,26 +64,43 @@ module.exports = {
       },
     },
     `gatsby-plugin-sharp`,
-    {
-      resolve: "gatsby-plugin-sitemap",
-      options: {
-        excludes: ["/chapter-*"],
-      },
-    },
-    {
-      resolve: "gatsby-plugin-manifest",
-      options: {
-        icon: "src/images/icon.png",
-      },
-    },
+    `gatsby-plugin-sitemap`,
+    `gatsby-plugin-offline`,
     {
       resolve: "gatsby-plugin-mdx",
       options: {
         defaultLayouts: {
           posts: require.resolve("./src/components/post-layout.tsx"),
           default: require.resolve("./src/components/page-layout.tsx")
-        }
+        },
+        gatsbyRemarkPlugins: [
+          {
+            resolve: `gatsby-remark-images`,
+            options: {
+              maxWidth: 550,
+            }
+          },
+        ],
+        rehypePlugins: [
+          {
+            resolve: 'rehype-slug'
+          }
+        ]
       }
+    },
+    {
+      resolve: `gatsby-plugin-google-gtag`,
+      options: {
+        trackingIds: [
+          'G-1DTH85EV1J'
+        ],
+        gtagConfig: {
+          anonymize_ip: true,
+        },
+        pluginConfig: {
+          head: false,
+        },
+      },
     },
   ],
 }
--- src/components/page-layout.tsx
+++ src/components/page-layout.tsx
@@ -1,74 +1,75 @@
 import * as React from "react";
 import { Helmet } from "react-helmet";
+import { useStaticQuery, graphql } from "gatsby";
 import clasNames from "classnames";
 
 import * as styles from "./page-layout.module.scss";
 
-const TITLE = 'Pět let zpět | Největší přešlapy současného prezidenta na časové ose';
-const DESC = 'Čeho všeho jsme byli svědky za posledních 5 let úřadování českého prezidenta na hradě? Nabízíme přehled novinových zpráv s největšími přešlapy na časové ose.';
-
-export const PageLayout = ({ children }) => (
-  <>
-    <Helmet
-      title={TITLE}
-      meta={[
-        {
-          name: `description`,
-          content: DESC,
-        },
-        {
-          property: `og:title`,
-          content: TITLE,
-        },
-        {
-          property: `og:description`,
-          content: DESC,
-        },
-        {
-          property: `og:type`,
-          content: `website`,
-        },
-        {
-          name: `twitter:card`,
-          content: `summary`,
-        },
-        {
-          name: `twitter:title`,
-          content: TITLE,
-        },
-        {
-          name: `twitter:description`,
-          content: DESC,
-        },
-      ]}
-      script={[
-        {
-          async: true,
-          src: 'https://www.googletagmanager.com/gtag/js?id=G-1DTH85EV1J',
-        },
-        { 
-          type: 'text/javascript', 
-          innerHTML: 'window.dataLayer = window.dataLayer || [];function gtag(){dataLayer.push(arguments)};gtag("js", new Date());gtag("config", "G-1DTH85EV1J");',
+export const PageLayout = ({ children }) => {
+  const { site: { siteMetadata }} = useStaticQuery(graphql`
+    query {
+      site {
+        siteMetadata {
+          title
+          description
         }
-      ]}
-    />
-    <Helmet
+      }
+    }
+  `);
+  const TITLE = `${siteMetadata.title} | Největší přešlapy současného prezidenta na časové ose`;
+  const DESC = siteMetadata.description;  
 
+  return (
+    <>
+      <Helmet
+        htmlAttributes={{ lang: 'en' }}
+        title={TITLE}
+        meta={[
+          {
+            name: `description`,
+            content: DESC,
+          },
+          {
+            property: `og:title`,
+            content: TITLE,
+          },
+          {
+            property: `og:description`,
+            content: DESC,
+          },
+          {
+            property: `og:type`,
+            content: `website`,
+          },
+          {
+            name: `twitter:card`,
+            content: `summary`,
+          },
+          {
+            name: `twitter:title`,
+            content: TITLE,
+          },
+          {
+            name: `twitter:description`,
+            content: DESC,
+          },
+        ]}
       />
-    <div className={styles.page}>
-      <header className={styles.header}>
-        <h1>{TITLE}</h1>
-      </header>
-      <main className={clasNames(styles.main, 'zeman')}>
-        {children}
-      </main>
-      <footer className={styles.footer}>
-        <small>
-          podporujeme: <a href="https://www.nasdilejneztozakazou.cz/">Sdílejte, než to zakážou! pravá tvář Andreje Babiše</a> a <a href="https://www.volby-kscm.cz/">Komunisti z kola ven</a>
-        </small>
-      </footer>
-    </div>
-  </>
-);
+      <div className={styles.page}>
+        <header className={styles.header}>
+          <h1>{TITLE}</h1>
+        </header>
+        <main className={clasNames(styles.main, 'zeman')}>
+          {children}
+        </main>
+        <footer className={styles.footer}>
+          <small>
+            podporujeme: <a href="https://www.nasdilejneztozakazou.cz/">Sdílejte, než to zakážou! pravá tvář Andreje Babiše</a> a <a href="https://www.volby-kscm.cz/">Komunisti z kola ven</a>
+          </small>
+        </footer>
+      </div>
+    </>
+  );
+};
 
 export default PageLayout;
